96a9f2e2f61f5459670d0603ba5609058e1d9cb1
BarbroNeedToSleep <55791445+BarbroNeedToSleep@users.noreply.github.com>
2025-06-12 17:32:15 +0200

    First work in java workshop. Simpe calculetaer with input from user. Still optinal work left to be done

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,114 @@
+package se.lexicon;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+//       Calculater that support
+//         addition
+//         subtraction
+//         multiplication
+//         division
+
+//         user input for said calculation
+//         user want to continue or end the program after calculation
+
+        System.out.println("Hello and welcome to the friendly calculator");
+        System.out.println("What operation for calculation do you like to use?");
+
+        boolean toContinue=false;
+        do {
+            System.out.println("\nOptions:");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. End Program");
+
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("What two numbers do you like to add together?");
+                    float a = scanner.nextFloat();
+                    System.out.println(" and ");
+                    float b = scanner.nextFloat();
+                    System.out.println(a + " + " + b + " = " + MathOperation.addition(a, b));
+
+                    toContinue = true;
+                    break;
+
+                    /*System.out.println("Would you like to make another calculation? (y/n?)");
+                    String input = scanner.nextLine();
+
+                    //switch (input) {
+                        case "n":
+                           // input.equalsIgnoreCase("n");
+                            toContinue=false;
+                            break;
+                        case "y":
+                          //
+                            toContinue =true;
+                            break;
+                        //case ” ”:
+                            //break;
+                   // }
+                   /*if(input.equalsIgnoreCase("n")) {
+                        break;
+                    } else if (input.equalsIgnoreCase("y")){
+                        toContinue = true;
+                        break;
+                    } else {
+                       System.out.println("Please enter y or n");
+                       break;
+                   }*/
+                case 2:
+                    System.out.println("What two numbers do you like to subtrack from another?");
+                    float c = scanner.nextFloat();
+                    System.out.println(" and ");
+                    float d = scanner.nextFloat();
+                    System.out.println(c + " - " + d + " = " + MathOperation.subtraction(c, d));
+
+                    toContinue = true;
+                    break;
+
+                case 3:
+                    System.out.println("What two numbers do you like to multiplay with one another?");
+                    float e = scanner.nextFloat();
+                    System.out.println(" and ");
+                    float f = scanner.nextFloat();
+                    System.out.println(e + " * " + f + " = " + MathOperation.multiplication(e, f));
+
+                    toContinue = true;
+                    break;
+
+                case 4:
+                    System.out.println("What number do you like to divide with another?");
+                    float g = scanner.nextFloat();
+                    System.out.println(" and ");
+                    float h = scanner.nextFloat();
+                    System.out.println(g + " / " + h + " = " + MathOperation.div(g, h));
+
+                    toContinue = true;
+                    break;
+
+                case 5:
+                    System.out.println("You have ended the program and ordered a Pizza!");
+                    toContinue = false;
+                    break;
+
+
+            }
+
+        } while (toContinue);
+
+
+
+
+
+    }
+}
---
+++ src/main/java/se/lexicon/MathOperation.java
@@ -0,0 +1,20 @@
+package se.lexicon;
+
+public class MathOperation {
+
+    public static float addition(float a, float b) {
+        return a + b;
+    }
+
+    public static float subtraction(float a, float b){
+        return a - b;
+    }
+
+    public static float multiplication(float a, float b) {
+        return a * b;
+    }
+
+    public  static float div(float a, float b){
+        return a / b;
+    }
+}
